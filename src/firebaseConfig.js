c40eddf99dea2feede7fbf1b97de30ed73bd9277
Er <ercj@ncbi.nlm.nih.gov>
2025-02-05 01:56:24 -0500

    added in the firebase config file for all related info to the web app on firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Import the functions you need from Firebase SDK
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJweCrJ491mfaVFeXLqSEpydCubc1TeA8",
+  authDomain: "deltaproject-2025.firebaseapp.com",
+  projectId: "deltaproject-2025",
+  storageBucket: "deltaproject-2025.firebasestorage.app",
+  appId: "1:945493200168:web:7f56b31c2d3eea1513941d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
